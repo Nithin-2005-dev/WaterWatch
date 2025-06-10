0d8b7fe1daee07254fe64db03caac2ec0a51e1b7
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2025-06-10 22:35:12 +0530

    waterwatch complete

--- frontend/src/components/Graphs.jsx
+++ frontend/src/components/Graphs.jsx
@@ -57,6 +57,7 @@ const Graphs = () => {
         const json = await res.json();
         const data = json.data || [];
         setLabels(data.map((d) => new Date(d.createdAt).toLocaleDateString()));
+        console.log(data)
         setReadings(data);
       } catch (err) {
         console.error("Failed to fetch data:", err);
@@ -237,7 +238,7 @@ const Graphs = () => {
   return (
     <div className="max-w-5xl mx-auto p-6 space-y-16 bg-gray-50 rounded-xl shadow-lg my-2">
       <h1 className="text-4xl md:text-5xl font-extrabold text-blue-700 mb-8 text-center drop-shadow-md border-b-4 border-blue-400 pb-2">
-        Analytics of NIT Silchar Lake 1
+        {`Analytics of ${readings[0].environment.name}`}
       </h1>
 
       {/* Normalized Line Chart */}
